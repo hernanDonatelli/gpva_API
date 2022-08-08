f8886e69c181da4bc5a0e22941eaf0665733b2a9
Hernan Donatelli <hernandonatelli@gmail.com>
2022-08-08 08:22:02 -0300

    agregado de proyecto a git

---
+++ index.js
@@ -0,0 +1,43 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e803178294mshff58520438cf35bp1ab069jsne123d38a4fa5",
+    "X-RapidAPI-Host": "formula-18.p.rapidapi.com",
+  },
+};
+
+let contenido = document.querySelector("#contenido");
+
+fetch("https://formula-18.p.rapidapi.com/raceResult?year=2022&race=13", options)
+  .then((response) => response.json())
+  .then((datos) => {
+      console.log(datos);
+
+      //Extraigo el objeto Results del array original y lo convierto en "Array de Resultados"
+      let resultados = datos.map(dato => dato.Results);
+      //console.log(resultados);
+
+      for (let i = 0; i < resultados.length; i++) {
+        let arrayResultados = resultados[i];
+        //console.log(arrayResultados)
+
+        for(let valor of arrayResultados){
+
+            contenido.innerHTML += `
+              <tr>
+                  <td>${valor.position}</td>
+                  <td>${valor.Driver.givenName} ${valor.Driver.familyName}</td>
+                  <td>${valor.Time ? valor.Time.time : valor.status}</td>
+                  <td>${valor.FastestLap.Time.time}</td>
+                  <td>${valor.laps}</td>
+                  <td>${valor.points}</td>
+              </tr>
+
+              `;
+
+        }
+      }
+
+    })
+  .catch((err) => console.error(err));
+
